16e6cdbee70e3660a5327f2b6c838cc712ee13d3
James Zhang <jjz002@ucsd.edu>
2025-05-16 15:53:48 -0700

    Add solution to week 7 di exercises

--- sqlite.go
+++ sqlite.go
@@ -2,48 +2,103 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+
 const dbPath = "students.db"
 
+
 func main() {
-	db := openDB()
-	defer db.Close()
+ db := openDB()
+ defer db.Close()
 
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: go run sqlite.go [add <id> <major> | read]")
-	}
 
-	command := os.Args[1]
+ if len(os.Args) < 2 {
+   log.Fatal("Usage: go run sqlite.go [add <id> <major> | read]")
+ }
 
-	switch command {
-	case "add":
-		if len(os.Args) < 4 {
-			log.Fatal("Usage: add <id> <major>")
-		}
-		addStudent(db, os.Args[2], os.Args[3])
-	case "read":
-		readStudents(db)
-	default:
-		log.Fatalf("Unknown command: %s", command)
-	}
+
+ command := os.Args[1]
+
+
+ switch command {
+ case "add":
+   if len(os.Args) < 4 {
+     log.Fatal("Usage: add <id> <major>")
+   }
+   addStudent(db, os.Args[2], os.Args[3])
+ case "read":
+   readStudents(db)
+ default:
+   log.Fatalf("Unknown command: %s", command)
+ }
 }
+
 
 // Task 1: Open the DB or create it if it does not exist
 func openDB() *sql.DB {
-	panic("todo")
+ _, err := os.Stat(dbPath)
+ dbExists := !errors.Is(err, os.ErrNotExist)
+
+
+ db, err := sql.Open("sqlite3", dbPath)
+ if err != nil {
+   log.Fatalf("failed to open database: %s", err)
+ }
+
+
+ if !dbExists {
+   createTableCommand := `
+     CREATE TABLE students (
+       id TEXT PRIMARY KEY,
+       major TEXT NOT NULL
+     );`
+   _, err := db.Exec(createTableCommand)
+   if err != nil {
+     db.Close()
+     log.Fatalf("failed to create students table: %s", err)
+   }
+   fmt.Println("Database and table created successfully.")
+ }
+
+
+ return db
 }
+
 
 // Task 2: Add a student record
 func addStudent(db *sql.DB, id string, major string) {
-	panic("todo")
+ _, err := db.Exec("INSERT INTO students (id, major) VALUES (?, ?)", id, major)
+ if err != nil {
+   log.Fatal("Failed to insert entry:", err)
+ }
+ fmt.Printf("Inserted student: %s\n", id)
 }
+
 
 // Task 3: Read all student records
 func readStudents(db *sql.DB) {
-	panic("todo")
+ rows, err := db.Query("SELECT id, major FROM students")
+ if err != nil {
+   log.Fatal("Failed to read from database:", err)
+ }
+ defer rows.Close()
+
+
+ fmt.Println("Student list:")
+ for rows.Next() {
+   var id string
+   var major string
+   if err := rows.Scan(&id, &major); err != nil {
+     log.Fatal(err)
+   }
+   fmt.Printf("ID: %s, Major: %s\n", id, major)
+ }
 }
+
